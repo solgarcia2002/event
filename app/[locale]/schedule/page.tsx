988c7bac14dd9ab778a077fc83c1fbcf38b44ffb
Rebeca Heredia <rbkheredia90@gmail.com>
2024-04-05 16:46:23 -0300

    navbar, pages

---
+++ app/[locale]/schedule/page.tsx
@@ -0,0 +1,8 @@
+const SchedulePage: React.FC = () =>{
+  return(
+    <>
+      <h1>Schedule Page</h1>
+    </>
+  )
+}
+export default SchedulePage;
---
+++ app/[locale]/travel&info/page.tsx
@@ -0,0 +1,8 @@
+const TravelPage: React.FC = () =>{
+  return(
+    <>
+      <h1>Travel & info Page</h1>
+    </>
+  )
+}
+export default TravelPage;
