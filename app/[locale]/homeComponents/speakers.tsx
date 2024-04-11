43da87ec398d9ce1c04a65af293bdcb1cd298df7
Rebeca Heredia <rbkheredia90@gmail.com>
2024-04-10 21:16:01 -0300

    speakers section

--- app/[locale]/homeComponents/speakers.tsx
+++ app/[locale]/homeComponents/speakers.tsx
@@ -1,28 +1,26 @@
-import "./speakers.css";
 import { speakers } from "../data/speakers";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+import "./speakers.css";
 
-import type { CollageImageProps } from "../components/collage/collage";
-import Collage from "../components/collage/collage";
 const Speakers: React.FC = () => {
-  const images: CollageImageProps[] = [
-    { src: "/speaker-angeles-lucero-bringas.png", animation: "right-to-left" },
-  ];
+  const t = useTranslations("home.speakersBio")
 
   return (
     <div className="cont-speakers">
-      <h2>Speakers</h2>
+      <h3>Speakers</h3>
       <div className="speakers-cont-cards">
         {speakers.map((item, i) => (
-          <div className="flip-card" key={i}>
-            <div className="flip-card-inner">
-              <div className="flip-card-front">
-                <Collage images={images} />
-                <p className="title">{item.name}</p>
-              </div>
-              <div className="flip-card-back">
-                <p className="title">{item.bio}</p>
-              </div>
+          <div className="card" key={i}>
+            <div className={`cont-text-card ${item.bio}`}>
+              <p className="card-name">{item.name}</p>
+              <br />
+              <p className="card-bio">{t(item.bio)}</p>
             </div>
+            <Image src={item.img} alt={item.name} width={800} height={1182} />
+            <video autoPlay loop muted playsInline className="video-speaker">
+              <source src={item.video} type="video/mp4" />
+            </video>
           </div>
         ))}
       </div>
