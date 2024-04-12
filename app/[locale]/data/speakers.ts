8b359255c780f69fd40ddcd6fa51593d4a02415d
Rebeca Heredia <rbkheredia90@gmail.com>
2024-04-12 18:41:17 -0300

    speakers angeles, claudia

--- app/[locale]/data/speakers.ts
+++ app/[locale]/data/speakers.ts
@@ -3,24 +3,28 @@ export const speakers = [
     name: "Angeles Lucero Bringas",
     img: "/speaker-angeles-lucero-bringas.png",
     bio: "angelesBio",
-    video:'/video-angeles.mp4'
+    video:'/video-angeles.mp4',
+    url:'/speakers/angeles'
   },
   { 
     name: "Claudia Lalloz", 
     img: "/speaker-claudia-lalloz.png", 
     bio: "claudiaBio" ,
-    video:'/video-claudia.mp4'
+    video:'/video-claudia.mp4',
+    url:'/speakers/claudia'
   },
   { 
     name: "Mirian Torrado", 
     img: "/speaker-mirian-torrado.png", 
     bio: "mirianBio" ,
-    video:'/video-mirian.mp4'
+    video:'/video-mirian.mp4',
+    url:'#'
   },
   { 
     name: "Tere Zazueta", 
     img: "/speaker-tere-zazueta.png", 
     bio: "tereBio",
-    video: '/video-tere-zazueta.mp4'
+    video: '/video-tere-zazueta.mp4',
+    url:'#'
   },
 ];
