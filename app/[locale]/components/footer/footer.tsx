18377bce5ce1cb0e34265077370b80ffb38f00e7
Rebeca Heredia <rbkheredia90@gmail.com>
2024-04-30 09:15:04 -0300

    agrego link mail footer

--- app/[locale]/components/footer/footer.tsx
+++ app/[locale]/components/footer/footer.tsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
 import { faFacebook, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import './footer.css';
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 const Footer:React.FC = () => {
 
   const router = useRouter();
@@ -19,6 +20,7 @@ const Footer:React.FC = () => {
     {title:'Facebook', url:'https://www.facebook.com/profile.php?id=61558330092580', icon:<FontAwesomeIcon icon={faFacebook} />},
     {title:'Instagram', url:'https://www.instagram.com/wellbizbridgemiami2024/', icon:<FontAwesomeIcon icon={faInstagram} />},
     {title:'Linkedin', url:'https://www.linkedin.com/company/102448014/admin/feed/posts/', icon:<FontAwesomeIcon icon={faLinkedin} />},
+    
   ]
 
   return(
@@ -33,6 +35,7 @@ const Footer:React.FC = () => {
             {item.icon}
           </Link>
         ))}
+        <Link className="link-socialM" href={'mailto:info@wellbizbridge.com'}><FontAwesomeIcon icon={faEnvelope} /></Link>
       </div>
       <div className="links-list">
         {links.map((item, i)=>(
