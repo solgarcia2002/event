c0c744a28130965c77c30c848377bb6811da1499
Rebeca Heredia <rbkheredia90@gmail.com>
2024-05-14 09:15:36 -0300

    agrego alts logos

--- app/[locale]/homeComponents/sponsorsList.tsx
+++ app/[locale]/homeComponents/sponsorsList.tsx
@@ -6,19 +6,19 @@ import "./sponsorsList.css";
 
 const SponsorsList: React.FC = () => {
   const logos = [
-    { logo: "/logo-santala.jpg", width: 325, height: 159 },
-    { logo: "/logo-angeles.png", width: 300, height: 300 },
-    { logo: "/logo-empowerit.png", width: 300, height: 300 },
-    { logo: "/logo-lalloz.png", width: 708, height: 147 },
-    { logo: "/logo-cesar.png", width: 248, height: 203 },
-    { logo: "/logo-susana.png", width: 110, height: 110 },
-    { logo: "/logo-latam.png", width: 110, height: 110 },
-    { logo: "/logo-camara.png", width: 110, height: 110 },
-    { logo: "/logo-fernando-socol.png", widht:400 , height:200 },
-    { logo: "/logo-ledesma.png", width: 110, height: 110 },
-    { logo: "/logo-trabi.png", width: 110, height: 110 },
-    { logo: "/chile-us.png", width: 110, height: 110 },
-    { logo: "/febicham.png", width: 110, height: 110 },
+    { logo: "/logo-santala.jpg", width: 325, height: 159, alt: 'santala' },
+    { logo: "/logo-angeles.png", width: 300, height: 300, alt: 'angeles-lucero-bringas' },
+    { logo: "/logo-empowerit.png", width: 300, height: 300, alt: 'empowerit.io' },
+    { logo: "/logo-lalloz.png", width: 708, height: 147, alt: 'lalloz' },
+    { logo: "/logo-cesar.png", width: 248, height: 203, alt: 'chayim' },
+    { logo: "/logo-susana.png", width: 110, height: 110, alt: 'susana-benavente' },
+    { logo: "/logo-latam.png", width: 110, height: 110, alt: 'latam-business-family' },
+    { logo: "/logo-camara.png", width: 110, height: 110, alt: 'aacc' },
+    { logo: "/logo-fernando-socol.png", widht:400 , height:200, alt: 'fernando-socol' },
+    { logo: "/logo-ledesma.png", width: 110, height: 110, alt: 'ledesma' },
+    { logo: "/logo-trabi.png", width: 110, height: 110, alt: 'trabi' },
+    { logo: "/chile-us.png", width: 110, height: 110, alt: 'chile-us' },
+    { logo: "/febicham.png", width: 110, height: 110, alt: 'febicham' },
   ];
   const tabStyle = {
     tabList: " gap-2 w-full relative p-2 border-b border-divider text-white",
@@ -36,7 +36,7 @@ const SponsorsList: React.FC = () => {
             <Image
               key={i}
               src={logo.logo}
-              alt="sponsor"
+              alt={logo.alt}
               width={325}
               height={159}
             />
